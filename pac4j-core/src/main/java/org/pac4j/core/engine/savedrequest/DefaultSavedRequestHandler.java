c9038575924216537fe6eec95053e7c821fea9ff
Vladimir Ivkovic <vlado.ivkovic@live.com>
2023-10-07 08:48:04 +0200

    Redirection URL after callback updated; version updated

--- pac4j-core/src/main/java/org/pac4j/core/engine/savedrequest/DefaultSavedRequestHandler.java
+++ pac4j-core/src/main/java/org/pac4j/core/engine/savedrequest/DefaultSavedRequestHandler.java
@@ -7,8 +7,6 @@ import org.pac4j.core.exception.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-
 /**
  * The default {@link SavedRequestHandler} which handles GET and POST requests.
  *
@@ -38,26 +36,9 @@ public class DefaultSavedRequestHandler implements SavedRequestHandler {
 
     @Override
     public HttpAction restore(final WebContext context, final String defaultUrl) {
-        final Optional<Object> optRequestedUrl = context.getSessionStore().get(context, Pac4jConstants.REQUESTED_URL);
-        HttpAction requestedAction = null;
-        if (optRequestedUrl.isPresent()) {
-            context.getSessionStore().set(context, Pac4jConstants.REQUESTED_URL, "");
-            final Object requestedUrl = optRequestedUrl.get();
-            if (requestedUrl instanceof FoundAction) {
-                requestedAction = (FoundAction) requestedUrl;
-            } else if (requestedUrl instanceof OkAction) {
-                requestedAction = (OkAction) requestedUrl;
-            }
-        }
-        if (requestedAction == null) {
-            requestedAction = new FoundAction(defaultUrl);
-        }
+        HttpAction requestedAction = new FoundAction(defaultUrl);
 
         LOGGER.debug("requestedAction: {}", requestedAction.getMessage());
-        if (requestedAction instanceof FoundAction) {
-            return RedirectionActionHelper.buildRedirectUrlAction(context, ((FoundAction) requestedAction).getLocation());
-        } else {
-            return RedirectionActionHelper.buildFormPostContentAction(context, ((OkAction) requestedAction).getContent());
-        }
+        return RedirectionActionHelper.buildRedirectUrlAction(context, ((FoundAction) requestedAction).getLocation());
     }
 }
